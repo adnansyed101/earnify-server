5f3912b88dee374ba094700b515280ef08e0934d
adnan <sirgeant10@gmail.com>
2025-01-14 21:45:28 +0600

    Create User model

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
