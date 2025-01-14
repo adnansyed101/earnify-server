b6d839e7ed82f8b2d5b499840e8e55f7a9973562
adnan <sirgeant10@gmail.com>
2025-01-14 21:31:57 +0600

    Add json,cors and connect to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello From Earnify");
