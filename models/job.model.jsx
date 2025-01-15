034358f4120fd252ac465a6aa53c834cda26fbc4
adnan <sirgeant10@gmail.com>
2025-01-15 15:09:54 +0600

    Create job model

---
+++ models/job.model.jsx
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  taskDetail: {
+    type: String,
+    required: true,
+  },
+  requiredWorkers: {
+    type: Number,
+    required: true,
+  },
+  payableAmount: {
+    type: Number,
+    required: true,
+  },
+  completionDate: {
+    type: Date,
+    required: true,
+  },
+  submissionInfo: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
