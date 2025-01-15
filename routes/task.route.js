ec94e9ea6ad107b4af3a476cd5c5803b7cd219b2
adnan <sirgeant10@gmail.com>
2025-01-15 20:17:52 +0600

    Rename job to task

---
+++ routes/task.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  createTask,
+  getAllTasks,
+  getSingleTask,
+} from "../controllers/task.controller.js";
+
+const router = express.Router();
+
+// Get all Tasks
+router.get("/", getAllTasks);
+
+// Create single Task
+router.get("/:id", getSingleTask);
+
+// Create a Task
+router.post("/", createTask);
+
+export default router;
