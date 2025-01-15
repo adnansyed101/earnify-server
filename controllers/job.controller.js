44ec7a8cff8c535c20c8f3b492dc6004bc8ba598
adnan <sirgeant10@gmail.com>
2025-01-15 18:46:31 +0600

    Get a single job

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Job from "../models/job.model.js";
 
 export const createJob = async (req, res) => {
@@ -31,12 +32,30 @@ export const createJob = async (req, res) => {
   }
 };
 
-export const getAllJobs = async (req, res) => {
+export const getAllTasks = async (req, res) => {
   try {
     const jobs = await Job.find({});
     res.status(200).json({ success: true, data: jobs });
   } catch (err) {
-    console.log("Error in creating job: " + err.message);
+    console.log("Error in finding all jobs: " + err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const getSingleTask = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Invalid Service Id" });
+  }
+
+  try {
+    const task = await Job.findById(id);
+    res.status(200).json({ success: true, data: task });
+  } catch (err) {
+    console.log("Error in finding job: " + err.message);
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
