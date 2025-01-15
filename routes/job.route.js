da68a2fad4230286217a703f604792a2349c0040
adnan <sirgeant10@gmail.com>
2025-01-15 18:09:21 +0600

    Add route to get all jobs

--- routes/job.route.js
+++ routes/job.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { createJob } from "../controllers/job.controller.js";
+import { createJob, getAllJobs } from "../controllers/job.controller.js";
 
 const router = express.Router();
+
+// Create a job
+router.get("/", getAllJobs);
 
 // Create a job
 router.post("/", createJob);
