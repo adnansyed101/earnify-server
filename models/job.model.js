8f9c25367dd45b6af8e583c717232ca83096e8e5
adnan <sirgeant10@gmail.com>
2025-01-15 17:01:05 +0600

    Create job model

--- models/job.model.js
+++ models/job.model.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
+const buyerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
 const jobSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -29,6 +44,7 @@ const jobSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  buyer: buyerSchema,
 });
 
 const Job = mongoose.model("Job", jobSchema);
