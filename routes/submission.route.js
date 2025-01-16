4849ca3a0060473970b64f9a69c8d11e22b6b7d0
adnan <sirgeant10@gmail.com>
2025-01-16 14:37:24 +0600

    Add route to get all submission

--- routes/submission.route.js
+++ routes/submission.route.js
@@ -1,10 +1,15 @@
-import express from "express"
-import { createSubmission } from "../controllers/submission.controller.js";
+import express from "express";
+import {
+  createSubmission,
+  getAllSubmissions,
+} from "../controllers/submission.controller.js";
 
-const router = express.Router()
+const router = express.Router();
+
+// Get All submissions
+router.get("/", getAllSubmissions);
 
 // Create Submission
 router.post("/", createSubmission);
 
-
-export default router
+export default router;
