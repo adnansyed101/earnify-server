3a49e4fd00c2113776590f58dd92ff9b8aa10555
adnan <sirgeant10@gmail.com>
2025-01-17 21:29:09 +0600

    Add route to get payments

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { createPayment } from "../controllers/payment.controller.js";
+import {
+  createPayment,
+  getUserPayments,
+} from "../controllers/payment.controller.js";
 
 const router = express.Router();
+
+// Get specific user payments
+router.get("/", getUserPayments);
 
 // Create Payment
 router.post("/", createPayment);
