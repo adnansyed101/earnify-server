21ca0b4ee8d51217b819ed486a4290639281e953
adnan <sirgeant10@gmail.com>
2025-01-18 23:06:11 +0600

    Create withdrawal model

---
+++ models/withdrawal.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const withdrawalSchema = new mongoose.Schema({
+  withdrawalCoin: {
+    type: Number,
+    required: true,
+  },
+  withdrawalAmount: {
+    type: String,
+    required: true,
+  },
+  paymentSystem: {
+    type: String,
+    required: true,
+  },
+  withdrawDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  workerEmail: {
+    type: String,
+    required: true,
+  },
+  worker: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
+});
+
+const Withdrawal = mongoose.model("Withdrawal", withdrawalSchema);
+
+export default Withdrawal;
