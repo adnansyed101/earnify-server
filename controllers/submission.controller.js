25637f64d3f17e6fb31efe65d2cbbee5520a6807
adnan <sirgeant10@gmail.com>
2025-01-18 13:22:27 +0600

    Add controller to to update submission status

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Submission from "../models/submission.model.js";
 
 export const createSubmission = async (req, res) => {
@@ -49,6 +50,29 @@ export const getAllSubmissions = async (req, res) => {
     res.status(200).json({ success: true, data: submissions });
   } catch (err) {
     console.log("Error in finding all submissions: " + err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+
+export const updateSubmissionStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(500).json({ success: false, message: "Invalid Task Id" });
+  }
+
+  const updated = {
+    $set: { status },
+  };
+
+  try {
+    const updateSubmission = await Submission.findByIdAndUpdate(id, updated, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updateSubmission });
+  } catch (err) {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
