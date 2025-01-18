1e7d8ee3bc06c11a03fb003b7ace77b9c698d48c
adnan <sirgeant10@gmail.com>
2025-01-18 13:22:50 +0600

    Add route to update submission status

--- routes/submission.route.js
+++ routes/submission.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createSubmission,
   getUserSubmissions,
+  updateSubmissionStatus,
 } from "../controllers/submission.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,8 @@ router.get("/", getUserSubmissions);
 
 // Create Submission
 router.post("/", createSubmission);
+
+// Update Submission Status
+router.patch("/update/status/:id", updateSubmissionStatus);
 
 export default router;
