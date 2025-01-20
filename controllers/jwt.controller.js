1358effc12cd715898e6ea9b7c48bf681a91d758
adnan <sirgeant10@gmail.com>
2025-01-20 16:21:18 +0600

    Create controllers for jwt

---
+++ controllers/jwt.controller.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+
+// Create JWT token and send to client
+export const createJWT = async (req, res) => {
+  const email = req.body;
+
+  const token = jwt.sign(email, process.env.SECRET_KEY, { expiresIn: "1d" });
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+};
+
+// Clear cookie on logout
+export const clearCookie = async (req, res) => {
+  res
+    .clearCookie("token", {
+      maxAge: 0,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+};
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    } else {
+      req.user = decoded;
+    }
+  });
+
+  next();
+};
