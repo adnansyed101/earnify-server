b93637a76cc4766ec7d954435cf412f35da7e6e9
adnan <sirgeant10@gmail.com>
2025-01-20 23:33:49 +0600

    Add route to get notificaiton

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createNotification } from "../controllers/notification.cotroller.js";
+import {
+  createNotification,
+  getUserNotifications,
+} from "../controllers/notification.cotroller.js";
 import { verifyToken } from "../controllers/jwt.controller.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createNotification);
+
+router.get("/", verifyToken, getUserNotifications);
 
 export default router;
