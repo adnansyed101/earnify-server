c55fb87ea07a053932d0c6351d7cbc814b834948
adnan <sirgeant10@gmail.com>
2025-01-20 16:20:26 +0600

    Create router for jwt

---
+++ routes/jwt.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { clearCookie, createJWT } from "../controllers/jwt.controller.js";
+
+const router = express.Router();
+
+// Generate Token
+router.post("/", createJWT);
+
+// Clear cookie on logout
+router.get("/logout", clearCookie);
+
+export default router;
